af8d51410062690f1e63cbae18aabeec3b70d726
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:22:09 +0300

    Initial commit

---
+++ Bankamatik Sistemi/Bankamatik Sistemi/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Bankamatik_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        #region Değişkenler
+        string yol = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=İşBankası.accdb";
+        string sorgu,sorgu2;
+        OleDbConnection bağlantı;
+        OleDbCommand komut,komut2;
+        OleDbDataAdapter kayıt,kayıt2;
+        DataTable tablo,tablo2;
+        int t;
+        int alınanpara;
+
+        int para,para1;
+        DateTime zaman;
+        #endregion
+
+        #region Metodlar
+
+        public void parayıver(int x)
+
+        {
+            parasayısı[0] = x / 200;
+            x = x % 200;
+            parasayısı[1] = x / 100;
+            x = x % 100;
+            parasayısı[2] = x / 50;
+            x = x % 50;
+            parasayısı[3] = x / 20;
+            x = x % 20;
+            parasayısı[4] = x / 10;
+            x = x % 10;
+            parasayısı[5] = x / 5;
+            x = x % 5;
+
+            if (x!=0)
+            {
+                MessageBox.Show("Şu anda bankamatikte bu işleminizi gerçekleştirecek banknot bulunmamaktadır","Bilgilendirme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                groupBox1.Visible = true;
+                label7.Text = parasayısı[0].ToString() + " Adet";
+                label6.Text = parasayısı[1].ToString() + " Adet";
+                label5.Text = parasayısı[2].ToString() + " Adet";
+                label4.Text = parasayısı[3].ToString() + " Adet";
+                label3.Text = parasayısı[4].ToString() + " Adet";
+                label2.Text = parasayısı[5].ToString() + " Adet";
+
+                
+            }
+        }
+
+        public void bağlan()
+        {
+
+            try
+            {
+                bağlantı = new OleDbConnection(yol);
+            }
+            catch (OleDbException ex)
+            {
+
+                MessageBox.Show(" " + ex, "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+        
+        }
+
+        public void kişibilgileri()
+        {
+            sorgu = "Select * from Kişiler";
+            komut = new OleDbCommand(sorgu,bağlantı);
+            kayıt = new OleDbDataAdapter(komut);
+            tablo = new DataTable();
+
+            bağlantı.Open();
+            kayıt.Fill(tablo);
+            bağlantı.Close();
+        
+        }
+
+        public void butondurumu()
+        {
+            if (para>=200)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+            }
+
+            else if (para>=100)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+
+            else if (para>=50)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+
+            else if (para>=20)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            else if (para>=10)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+
+            else if (para>=5)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        public void butonfalse()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+        
+        
+        }
+
+        public void paragüncelle()
+        {
+            zaman = DateTime.Now;
+            sorgu2 = "Update Kişiler set Karttaki_Miktar='"+para+"', Son_Islem='"+zaman+"' where Kimlik="+(t+1)+"";
+            komut2 = new OleDbCommand(sorgu2,bağlantı);
+            bağlantı.Open();
+            komut2.ExecuteNonQuery();
+            bağlantı.Close();
+        }
+
+        public void labeltemizle()
+        {
+            label2.Text = "?";
+            label3.Text = "?";
+            label4.Text = "?";
+            label5.Text = "?";
+            label6.Text = "?";
+            label7.Text = "?";
+            textBox1.Clear();
+        }
+        
+
+        #endregion
+
+       
+
+        #region Tanımlamalar
+
+        int[] parasayısı=new int[6];
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            butonfalse();
+            bağlan();
+            kişibilgileri();
+            t = Giriş.i;
+            para = Convert.ToInt32(tablo.Rows[t]["Karttaki_Miktar"]);
+            label11.Text = tablo.Rows[t]["Karttaki_Miktar"].ToString()+" TL";
+            label13.Text = tablo.Rows[t]["Son_Islem"].ToString().Substring(0, 9);
+            timer1.Start();
+            groupBox1.Visible = false;
+           
+            label9.Text = Giriş.ad + " " + Giriş.soyad;
+            butondurumu();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label15.Text = DateTime.Now.ToLongTimeString();
+            label14.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                alınanpara = Convert.ToInt32(textBox1.Text);
+                parayıver(alınanpara);
+                
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Lütfen Geçerli Bir Değer Giriniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button1.Text.Substring(0, 1)));
+            alınanpara = int.Parse(button1.Text.Substring(0, 1));
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button2.Text.Substring(0, 2)));
+            alınanpara = int.Parse(button2.Text.Substring(0, 2));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button3.Text.Substring(0, 2)));
+            alınanpara = int.Parse(button3.Text.Substring(0, 2));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button4.Text.Substring(0, 3)));
+            alınanpara = int.Parse(button4.Text.Substring(0, 2));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button5.Text.Substring(0, 3)));
+            alınanpara = int.Parse(button5.Text.Substring(0, 3));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            parayıver(int.Parse(button6.Text.Substring(0, 3)));
+            alınanpara = int.Parse(button6.Text.Substring(0, 3));
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            para = para - alınanpara;
+            bağlan();
+            paragüncelle();
+            kişibilgileri();
+            para = Convert.ToInt32(tablo.Rows[t]["Karttaki_Miktar"]);
+            label11.Text = tablo.Rows[t]["Karttaki_Miktar"].ToString() + " TL";
+            label13.Text = tablo.Rows[t]["Son_Islem"].ToString();
+            butonfalse();
+            butondurumu();
+            labeltemizle();
+        }
+    }
+}
---
+++ Bankamatik Sistemi/Bankamatik Sistemi/Giriş.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Bankamatik_Sistemi
+{
+    partial class Giriş
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, -2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(265, 116);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(257, 87);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Yönetici Girişi";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(9, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 26);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Yönetici Numarasıı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(118, 20);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(132, 21);
+            this.textBox2.TabIndex = 26;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Size = new System.Drawing.Size(257, 87);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Müşteri Girişi";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(12, 48);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(237, 28);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "Giriş";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Kart Numarası";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(96, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(153, 21);
+            this.textBox1.TabIndex = 23;
+            // 
+            // Giriş
+            // 
+            this.AcceptButton = this.button7;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(266, 117);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Giriş";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş";
+            this.Load += new System.EventHandler(this.Giriş_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+
+
+
+    }
+}
